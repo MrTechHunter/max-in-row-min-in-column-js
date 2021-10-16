7fe41ab07008be4f4329bf44053441bb0a44e7b3
MrTechHunter <this.is.techhunter@gmail.com>
2021-10-16 17:28:03 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let matrix = ([[21, 11, 13, 3],
+    [4, 1, 22, 30],
+    [41, 50, 8, 91],
+    [10, 7, 34, 17]])
+
+for(let i = 0; i < matrix.length; i++){
+    let max = Math.max(i)
+    let min = Math.min(i)
+
+    console.log(max)
+    console.log(min)
+}
